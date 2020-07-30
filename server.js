0693c7517ff6b1ff4a6ce1ded45cf3681397066d
pranith9810 <n.rao@mountblue.tech>
2020-07-30 10:56:41 +0530

    Http server in server.js file now gives response for the request url '/html' by rendering the index.html file

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+
+const promiseReadFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+};
 
 const server = http.createServer((req, res) => {
-    res.write("Hiiiiii");
-    res.end();
-})
+    switch (req.url) {
+        case '/html':
+            promiseReadFile('./index.html')
+                .then((data) => {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    })
+                    res.write(data);
+                    res.end();
+                })
+                .catch((err) => {
+                    console.error('Error while reading the index.html file ' + err);
+                });
+            break;
+
+        default:
+            res.writeHead(404);
+            res.write('404 File Not Found!!');
+            res.end();
+    }
+});
 
 server.listen(8000);
